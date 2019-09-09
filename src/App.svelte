5f0ff2a810dfe975423739966ab058c2447180ae
igor0ser <igor0ser@gmail.com>
2019-09-09 17:35:53 +0300

    Init project and add materialize lib to build

---
+++ src/App.svelte
@@ -0,0 +1,15 @@
+<script>
+    import '../node_modules/materialize-css/dist/css/materialize.css'
+    import '../node_modules/materialize-css/dist/js/materialize.js'
+	export let name;
+</script>
+
+<style>
+	h1 {
+		color: yellow;
+		background-color: #333;
+		padding: 20px;
+	}
+</style>
+
+<h1>Hello, {name}1!</h1>
