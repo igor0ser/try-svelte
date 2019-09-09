ec82f7fa6f799b46b0ff6b056c302e19b6226706
igor0ser <igor0ser@gmail.com>
2019-09-09 18:00:23 +0300

    Routing added

---
+++ src/About.svelte
@@ -0,0 +1,3 @@
+<h1>
+  About page here
+</h1>
--- src/App.svelte
+++ src/App.svelte
@@ -1,15 +1,28 @@
 <script>
-    import '../node_modules/materialize-css/dist/css/materialize.css'
-    import '../node_modules/materialize-css/dist/js/materialize.js'
-	export let name;
+  import '../node_modules/materialize-css/dist/css/materialize.css'
+  import '../node_modules/materialize-css/dist/js/materialize.js'
+
+  import { Router, Route } from 'svelte-routing';
+  import Navbar from './Navbar.svelte'
+  import Home from './Home.svelte'
+  import About from './About.svelte'
+
+  export let name;
 </script>
 
 <style>
-	h1 {
-		color: yellow;
-		background-color: #333;
-		padding: 20px;
-	}
+  h1 {
+    color: yellow;
+    background-color: #333;
+    padding: 20px;
+  }
 </style>
 
-<h1>Hello, {name}1!</h1>
+<Router>
+  <Navbar />
+  <div class="container">
+    <Route path="/" component={Home} />
+    <Route path="/about" component={About} />
+  </div>
+</Router>
+
